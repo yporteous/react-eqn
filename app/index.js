2178fabb20e5f1fb32b2f617b1e68ce2f17d47a9
yporteous <ghazalla>
2016-11-30 13:07:03 +0000

    Props eliminated

--- app/index.js
+++ app/index.js
@@ -4,6 +4,6 @@ var ReactDOM = require('react-dom');
 var Expression = require("./components/Expression");
 
 ReactDOM.render(
-	<Expression terms={[1,4,2]} />,
+	<Expression />,
 	document.getElementById('app')
 );
