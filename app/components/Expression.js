9e07693c044754fead6538d5362378fc5b0da075
yporteous <ghazalla>
2016-12-14 10:58:11 +0000

    Finished Term component (not yet in use)

--- app/components/Expression.js
+++ app/components/Expression.js
@@ -2,11 +2,42 @@ var React = require('react');
 
 var Term = React.createClass({
 	render: function () {
-		let coefficient = 2;
-		let index = 1;
-		return (<p>{coefficient} &times; x <sup>index</sup></p>);
+		let coeff = Number(this.props.coefficient);
+		let index = this.props.index;
+		let sign = "+";
+		if (coeff < 0) {
+			sign = "–";
+			coeff *= -1;
+		}
+		if (coeff === 1) {
+			coeff = ""
+		} else {
+			coeff = coeff.toString();
+		}
+		switch (index) {
+			case 0:
+				if (coefficient) {
+					return (<span key={props.key}>{sign}{coefficient}</span>);
+				} else {
+					return null;
+				}
+				break;
+			case 1:
+				if (coefficient === 1) {
+					return (<span key={props.key}>{sign}<em>x</em></span>);
+				} else {
+					return (<span key={props.key}>{sign}{coefficient}<em>x</em></span>);
+				}
+				break;
+			default:
+			if (coefficient === 1) {
+				return (<span key={props.key}>{sign}<em>x</em><sup>{index}</sup></span>);
+			} else {
+				return (<span key={props.key}>{sign}{coefficient}<em>x</em><sup>{index}</sup></span>);
+			}
+		}
 	}
-})
+});
 
 var Expression = React.createClass({
 	render: function () {
