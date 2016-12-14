e85443627bbe41afa8daf4d46124695110975e41
yporteous <ghazalla>
2016-12-14 12:55:19 +0000

    Properly omitting first sign if +ve

--- app/components/Expression.js
+++ app/components/Expression.js
@@ -3,55 +3,69 @@ var React = require('react');
 var Term = React.createClass({
 	render: function () {
 		let coeff = Number(this.props.coefficient);
-		let index = this.props.key;
+		let index = this.props.index;
 		let sign = "+";
+		if (this.props.first) {
+			sign = "";
+		}
 		if (coeff < 0) {
 			sign = "–";
 			coeff *= -1;
 		}
 		if (coeff === 1) {
-			coeff = ""
+			coeff = "";
 		} else {
 			coeff = coeff.toString();
 		}
 		switch (index) {
 			case 0:
-				if (coefficient) {
-					return (<span key={props.key}>{sign}{coefficient}</span>);
+				if (coeff) {
+					return (<span key={index}>{sign}{coeff}</span>);
 				} else {
 					return null;
 				}
 				break;
 			case 1:
-				if (coefficient === 1) {
-					return (<span key={this.props.key}>{sign}<em>x</em></span>);
+				if (coeff === 1) {
+					return (<span key={index}>{sign}<em>x</em></span>);
 				} else {
-					return (<span key={this.props.key}>{sign}{coefficient}<em>x</em></span>);
+					return (<span key={index}>{sign}{coeff}<em>x</em></span>);
 				}
 				break;
 			default:
-			if (coefficient === 1) {
-				return (<span key={props.key}>{sign}<em>x</em><sup>{index}</sup></span>);
-			} else {
-				return (<span key={props.key}>{sign}{coefficient}<em>x</em><sup>{index}</sup></span>);
-			}
+				if (coeff === 1) {
+					return (<span key={index}>{sign}<em>x</em><sup>{index}</sup></span>);
+				} else {
+					return (<span key={index}>{sign}{coeff}<em>x</em><sup>{index}</sup></span>);
+				}
 		}
 	}
 });
 
 var Expression = React.createClass({
 	render: function () {
-		if (!this.props.terms.length) {
+		let terms = this.props.terms;
+		let last = terms.length - 1;
+
+		if (terms[-1] === 0) {
+			while (terms[last] === 0) {
+				last--;
+			}
+			last++;
+			terms.splice(last, terms.length - last);
+		}
+
+		if (!terms.length) {
 			return (<p>0</p>);
 		}
 		else {
 			return (
 				<p>
-					{this.props.terms.slice(0).reverse().map((coeff, index) => (
-						<Term key={index.toString()} coefficient={coeff} />
+					{terms.slice(0).reverse().map((coeff, index) => (
+						<Term key={index.toString()} first={!index} index={terms.length-index-1} coefficient={coeff} />
 					))}
 				</p>
-			)
+			);
 		}
 	}
 });
