6069728baf39cb7dc02535e69c0d4c4fc8f6ef6b
yporteous <ghazalla>
2016-12-25 02:38:47 +0000

    Now correctly mutating this.state instead of this.props

--- app/index.js
+++ app/index.js
@@ -8,10 +8,16 @@ function Side() {
 }
 
 Side.prototype.add = function (index, value) {
-	while (typeof(this.terms.index) === "undefined") {
+	while (this.terms.length < index) {
 		this.terms.push(0);
 	}
 	this.terms[index] += value;
+};
+
+Side.prototype.scale = function (factor) {
+	for (var i = 0; i < this.terms.length; i++) {
+		this.terms[i] *= factor;
+	}
 };
 
 Side.prototype.set = function (terms) {
@@ -21,20 +27,43 @@ Side.prototype.set = function (terms) {
 var lhs = new Side();
 var rhs = new Side();
 
-lhs.set([1,2,3]);
+lhs.set([1,2]);
 rhs.set([2,1]);
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			lhs: this.props.lhs,
+			rhs: this.props.rhs
+		};
 		this.add = this.add.bind(this);
 	}
 
 	add(index, value, toBoth) {
+		var rhs = this.state.rhs;
+		rhs.add(index, value);
+		var lhs = this.state.lhs;
 		if (toBoth) {
-			this.props.lhs.add(index, value);
+			lhs.add(index, value);
 		}
-		this.props.rhs.add(index, value);
+		this.setState({
+			lhs: lhs,
+			rhs: rhs
+		});
+	}
+
+	scale(factor, toBoth) {
+		var rhs = this.state.rhs;
+		rhs.scale(factor);
+		var lhs = this.state.lhs;
+		if (toBoth) {
+			lhs.scale(factor);
+		}
+		this.setState({
+			lhs: lhs,
+			rhs: rhs
+		});
 	}
 
 	render() {
@@ -45,36 +74,12 @@ class App extends React.Component {
 				<Expression terms={this.props.rhs.terms}/>
 			</p>
 			<p>
-				<button onClick={this.add(0,1,true)}>+1</button>
+				<button onClick={() => this.add(0,1,true)}>+1</button>
+				<button onClick={() => this.scale(2,true)}>&times;2</button>
 			</p>
 		</div>);
 	}
 }
-
-/*/
-var App = React.createClass({
-	add: function(index, value, toBoth) {
-		if (toBoth) {
-			this.props.lhs.add(index, value);
-		}
-		this.props.rhs.add(index, value);
-	},
-	render: function() {
-		return (
-			<div>
-				<p>
-					<Expression terms={this.props.lhs.terms}/>
-					<span> = </span>
-					<Expression terms={this.props.rhs.terms}/>
-				</p>
-				<p>
-					<button>+1</button>
-				</p>
-			</div>
-		);
-	}
-});
-//*/
 
 ReactDOM.render(
 	<App lhs={lhs} rhs={rhs} />,
